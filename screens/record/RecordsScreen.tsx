51784a0f634042a6ed9ac76dd11df3ec7b469bef
minimalKim <kimin3004@gmail.com>
2023-08-01 23:54:25 +0900

    ✨ feat: RecordsScreen 및 하위 TabScreen 추가

--- screens/record/RecordsScreen.tsx
+++ screens/record/RecordsScreen.tsx
@@ -1,19 +1,41 @@
-import {SafeAreaView, Text, Button} from 'react-native';
-import {BottomTabStackParamList, RootStackParamList} from '../../navigators';
+import {SafeAreaView} from 'react-native';
 import {CompositeScreenProps} from '@react-navigation/native';
 import {BottomTabScreenProps} from '@react-navigation/bottom-tabs';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
+import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
+
+import {BottomTabStackParamList, RootStackParamList} from '../../navigators';
+
+import {RecordsTabScreen} from './RecordsTabScreen';
+import {SummaryTabScreen} from './SummaryTabScreen';
 
 type Props = CompositeScreenProps<
   BottomTabScreenProps<BottomTabStackParamList, 'Records'>,
   NativeStackScreenProps<RootStackParamList>
 >;
 
+const Tab = createMaterialTopTabNavigator();
+
 export function RecordsScreen({navigation}: Props) {
   return (
-    <SafeAreaView>
-      <Text>RecordsScreen</Text>
-      <Button title="건강앱 연동하기" />
-    </SafeAreaView>
+    <>
+      <SafeAreaView style={{backgroundColor: '#ffffff'}} />
+      <Tab.Navigator>
+        <Tab.Screen
+          name="RecordsTab"
+          options={{
+            tabBarLabel: '운동 기록',
+          }}
+          component={RecordsTabScreen}
+        />
+        <Tab.Screen
+          name="SummaryTab"
+          options={{
+            tabBarLabel: '요약',
+          }}
+          component={SummaryTabScreen}
+        />
+      </Tab.Navigator>
+    </>
   );
 }
---
+++ screens/record/RecordsTabScreen.tsx
@@ -0,0 +1,9 @@
+import {View, Text} from 'react-native';
+
+export function RecordsTabScreen() {
+  return (
+    <View>
+      <Text>운동 기록 뷰</Text>
+    </View>
+  );
+}
