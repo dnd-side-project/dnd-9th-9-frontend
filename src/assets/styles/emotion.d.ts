6eef10207fa3f862f5d8d65e466a24586631067b
chajuhui123 <chajuhui123@naver.com>
2023-08-03 13:46:06 +0900

    💄 feat: emotion theme 활용한 디자인 시스템 셋팅 (#7)

---
+++ src/assets/styles/emotion.d.ts
@@ -0,0 +1,53 @@
+import '@emotion/react';
+
+export type Tpalette =
+  | 'main-400'
+  | 'main-300'
+  | 'main-200'
+  | 'main-100'
+  | 'error-light'
+  | 'error-dark'
+  | 'sub-400'
+  | 'gray-0'
+  | 'gray-50'
+  | 'gray-100'
+  | 'gray-200'
+  | 'gray-300'
+  | 'gray-400'
+  | 'gray-500'
+  | 'gray-600'
+  | 'gray-700'
+  | 'gray-800'
+  | 'gray-900'
+  | 'gray-950'
+  | black;
+
+export type Ttypography =
+  | 'head1'
+  | 'head2'
+  | 'head3'
+  | 'head4'
+  | 'body1'
+  | 'body2'
+  | 'body3'
+  | 'caption';
+
+export type TBorderRadius = 'sm' | 'md' | 'lg';
+
+declare module '@emotion/react' {
+  export interface Theme {
+    palette: {
+      [key in Tpalette]: string;
+    };
+    typography: {
+      [key in Ttypography]: {
+        fontFamily: string;
+        fontWeight: string | number;
+        fontSize: string | number;
+      };
+    };
+    borderRadius: {
+      [key in TBorderRadius]: string;
+    };
+  }
+}
---
+++ src/assets/styles/theme.ts
@@ -0,0 +1,77 @@
+const palette = {
+  'main-400': '#01D281',
+  'main-300': '#4EE0A7',
+  'main-200': '#80E9C0',
+  'main-100': '#B3F2DA',
+  'error-light': '#FF6565',
+  'error-dark': '#FF4646',
+  'sub-400': '#C781FF',
+  'gray-0': '#FFFFFF',
+  'gray-50': '#F7F7FA',
+  'gray-100': '#F0F0F5',
+  'gray-200': '#E8E8EE',
+  'gray-300': '#E1E1E8',
+  'gray-400': '#CDCED6',
+  'gray-500': '#A9ABB8',
+  'gray-600': '#858899',
+  'gray-700': '#525463',
+  'gray-800': '#3E404C',
+  'gray-900': '#2B2D36',
+  'gray-950': '#252730',
+  black: '#000000',
+};
+
+const typography = {
+  head1: {
+    fontFamily: 'Pretendard',
+    fontWeight: 'bold',
+    fontSize: '34px',
+  },
+  head2: {
+    fontFamily: 'Pretendard',
+    fontWeight: 'bold',
+    fontSize: '26px',
+  },
+  head3: {
+    fontFamily: 'Pretendard',
+    fontWeight: 'semibold',
+    fontSize: '24px',
+  },
+  head4: {
+    fontFamily: 'Pretendard',
+    fontWeight: 'semibold',
+    fontSize: '20px',
+  },
+  body1: {
+    fontFamily: 'Pretendard',
+    fontWeight: 'regular',
+    fontSize: '18px',
+  },
+  body2: {
+    fontFamily: 'Pretendard',
+    fontWeight: 'regular',
+    fontSize: '16px',
+  },
+  body3: {
+    fontFamily: 'Pretendard',
+    fontWeight: 'regular',
+    fontSize: '14px',
+  },
+  caption: {
+    fontFamily: 'Pretendard',
+    fontWeight: 'regular',
+    fontSize: '12px',
+  },
+};
+
+const borderRadius = {
+  sm: '8px',
+  md: '12px',
+  lg: '22px',
+};
+
+export const theme = {
+  palette,
+  typography,
+  borderRadius,
+};
