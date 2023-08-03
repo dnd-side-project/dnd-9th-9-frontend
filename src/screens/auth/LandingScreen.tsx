b1b232c4cfbeed9f3bdcba0319eb888751215507
chajuhui123 <chajuhui123@naver.com>
2023-08-03 13:50:00 +0900

    🚧 feat: Button, Text 컴포넌트 임시 코드 작성 (#7)

--- src/screens/auth/LandingScreen.tsx
+++ src/screens/auth/LandingScreen.tsx
@@ -1,15 +1,30 @@
-import {Button, Text, SafeAreaView} from 'react-native';
+import {SafeAreaView} from 'react-native';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import {RootStackParamList} from '../../navigators';
+import {Text} from '../../components/Text';
+import {Button} from '../../components/Button';
 
 type Props = NativeStackScreenProps<RootStackParamList, 'Landing'>;
 
 export function LandingScreen({navigation}: Props) {
   return (
     <SafeAreaView>
-      <Text>LandingScreen</Text>
-      <Button title="로그인" onPress={() => navigation.push('Login')} />
-      <Button title="회원가입" onPress={() => navigation.push('Signup')} />
+      <Text text="TITLE TEST 1" type="head1" />
+      <Text text="TITLE TEST 2" type="head2" />
+      <Text text="TITLE TEST 3" type="head3" />
+
+      <Button
+        text="로그인"
+        size="sm"
+        disabled={true}
+        onPress={() => navigation.push('Login')}
+      />
+      <Button
+        text="회원가입"
+        size="md"
+        disabled={false}
+        onPress={() => navigation.push('Signup')}
+      />
     </SafeAreaView>
   );
 }
