6176a9b3c596bca40f279b6e28986a442d7f0e00
minimalKim <kimin3004@gmail.com>
2023-08-05 13:15:35 +0900

    📝 chore: 패키지 background 및 access 관련 설정 추가

--- ios/HealthMatchingProject/AppDelegate.mm
+++ ios/HealthMatchingProject/AppDelegate.mm
@@ -2,6 +2,8 @@
 
 #import <React/RCTBundleURLProvider.h>
 
+#import "RCTAppleHealthKit.h"
+
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
@@ -10,6 +12,11 @@
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
+  
+  RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
+  
+  /* Add Background initializer for HealthKit  */
+  [[RCTAppleHealthKit new] initializeBackgroundObservers:bridge];
 
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
