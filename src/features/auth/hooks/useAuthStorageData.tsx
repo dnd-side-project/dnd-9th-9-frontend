4411d98f5e3966ae752791ca85b0ed61feb2f98a
minimalKim <kimin3004@gmail.com>
2023-08-06 20:13:09 +0900

    ✨ feat: 예시코드 추가

--- src/features/auth/hooks/useAuthStorageData.tsx
+++ src/features/auth/hooks/useAuthStorageData.tsx
@@ -1,10 +1,13 @@
-import {useAsyncStorage} from '@react-native-async-storage/async-storage';
+import AsyncStorage, {
+  useAsyncStorage,
+} from '@react-native-async-storage/async-storage';
 import {useEffect, useState} from 'react';
 
 const KEY = 'auth-jwt-token';
 
 // example code
-export function useAuthStorageData() {
+// 1. hook
+export function useAuthStorage() {
   const [token, setToken] = useState<string | null>();
   const {getItem, setItem} = useAsyncStorage(KEY);
 
@@ -27,3 +30,22 @@ export function useAuthStorageData() {
     setToken: setTokenFromStorage,
   };
 }
+
+// 2. obj
+export const authStorage = {
+  async get() {
+    try {
+      const jsonToken = await AsyncStorage.getItem(KEY);
+      return jsonToken ? JSON.parse(jsonToken) : null;
+    } catch (e) {
+      throw new Error('Failed to load token');
+    }
+  },
+  async set(data: string) {
+    try {
+      await AsyncStorage.setItem(KEY, JSON.stringify(data));
+    } catch (e) {
+      throw new Error('Failed to save token');
+    }
+  },
+};
