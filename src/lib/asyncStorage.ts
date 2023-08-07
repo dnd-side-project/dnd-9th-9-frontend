0d88040758ac6c6fea0da7c44d42fa3968fffb7b
Minjeong Kim <80511900+minimalKim@users.noreply.github.com>
2023-08-07 21:28:06 +0900

    📝 chore: async-storage 버전 업데이트 및 예시 코드 추가 (#17)
    * 📝 chore: @react-native-async-storage/async-storage 마이너 버전 업데이트
    
    * ✨ feat: 예시 코드 추가
    
    * ✨ feat: 예시코드 추가
    
    * 📝 chore: 디렉토리 변경
    
    * 📝 chore: 파일명 변경
    
    * ✨ feat: utility type 추가
    
    * ✨ feat: asyncStorage 객체 생성
    
    * 📝 chore: example code 수정
    
    * 📝 chore: lib 디렉토리로 이동
    
    * 🔨 refactor: prefix 및 console.error 리팩토링
    
    * 📝 chore: 불필요 예시코드 제거
    
    ---------
    
    Co-authored-by: minimalKim <kimin3004@gmail.com>

---
+++ src/lib/asyncStorage.ts
@@ -0,0 +1,87 @@
+import AsyncStorage, {
+  useAsyncStorage as _useAsyncStorage,
+} from '@react-native-async-storage/async-storage';
+import {ValueOf} from '../utils/types';
+import {AsyncStorageHook} from '@react-native-async-storage/async-storage/lib/typescript/types';
+
+const PREFIX = 'matchup' as const;
+
+export const ASYNC_STORAGE_KEYS = {
+  AUTH_JWT: 'auth-jwt-token',
+} as const;
+
+type TAsyncStorageKey = ValueOf<typeof ASYNC_STORAGE_KEYS>;
+type TErrorHandlingFunction = (error: Error, key?: TAsyncStorageKey) => void;
+
+const defaultErrorHandler: TErrorHandlingFunction = error => {
+  console.error(error.message);
+};
+
+export const asyncStorage = {
+  get: async <T>(
+    key: TAsyncStorageKey,
+    errorHandler: TErrorHandlingFunction = defaultErrorHandler,
+  ): Promise<T | null> => {
+    try {
+      const data = await AsyncStorage.getItem(`${PREFIX}-${key}`);
+      return data ? (JSON.parse(data) as T) : null;
+    } catch (error) {
+      if (error instanceof Error) {
+        errorHandler(error, key);
+      } else {
+        console.error(`AsyncStorage Error: get ${key}`);
+      }
+      return null;
+    }
+  },
+
+  set: async <T>(
+    key: TAsyncStorageKey,
+    value: T,
+    errorHandler: TErrorHandlingFunction = defaultErrorHandler,
+  ): Promise<void> => {
+    try {
+      const data = JSON.stringify(value);
+      await AsyncStorage.setItem(`${PREFIX}-${key}`, data);
+    } catch (error) {
+      if (error instanceof Error) {
+        errorHandler(error, key);
+      } else {
+        console.error(`AsyncStorage Error: set ${key}`);
+      }
+    }
+  },
+
+  remove: async (
+    key: TAsyncStorageKey,
+    errorHandler: TErrorHandlingFunction = defaultErrorHandler,
+  ): Promise<void> => {
+    try {
+      await AsyncStorage.removeItem(`${PREFIX}-${key}`);
+    } catch (error) {
+      if (error instanceof Error) {
+        errorHandler(error, key);
+      } else {
+        console.error(`AsyncStorage Error: remove ${key}`);
+      }
+    }
+  },
+
+  clear: async (
+    errorHandler: TErrorHandlingFunction = defaultErrorHandler,
+  ): Promise<void> => {
+    try {
+      await AsyncStorage.clear();
+    } catch (error) {
+      if (error instanceof Error) {
+        errorHandler(error);
+      } else {
+        console.error(`AsyncStorage Error: clear `);
+      }
+    }
+  },
+};
+
+export const useAsyncStorage = (key: TAsyncStorageKey): AsyncStorageHook => {
+  return _useAsyncStorage(`${PREFIX}-${key}`);
+};
