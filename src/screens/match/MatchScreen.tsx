4c14a47a075c1df0a3614e4fe08424e85f091eb2
minimalKim <kimin3004@gmail.com>
2023-08-08 23:53:38 +0900

    ✨ feat: TopTabNavigator 컴포넌트 사용

--- src/screens/match/MatchScreen.tsx
+++ src/screens/match/MatchScreen.tsx
@@ -2,40 +2,38 @@ import {SafeAreaView} from 'react-native';
 import {CompositeScreenProps} from '@react-navigation/native';
 import {BottomTabScreenProps} from '@react-navigation/bottom-tabs';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
-import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
 
 import {BottomTabStackParamList, RootStackParamList} from '../../navigators';
 
 import {TotalMatchListTabScreen} from './TotalMatchListTabScreen';
 import {MyMatchListTabScreen} from './MyMatchListTabScreen';
 
+import {TopTabNavigator} from '../../components/TopTabNavigator';
+import {theme} from '../../assets/styles/theme';
+
 type Props = CompositeScreenProps<
   BottomTabScreenProps<BottomTabStackParamList, 'Match'>,
   NativeStackScreenProps<RootStackParamList>
 >;
 
-const Tab = createMaterialTopTabNavigator();
+const screens = [
+  {
+    name: 'TotalMatchListTab',
+    label: '매칭',
+    component: TotalMatchListTabScreen,
+  },
+  {
+    name: 'MyMatchListTab',
+    label: 'MY매칭',
+    component: MyMatchListTabScreen,
+  },
+];
 
 export function MatchScreen({navigation}: Props) {
   return (
     <>
-      <SafeAreaView style={{backgroundColor: '#ffffff'}} />
-      <Tab.Navigator>
-        <Tab.Screen
-          name="TotalMatchListTab"
-          options={{
-            tabBarLabel: '매칭',
-          }}
-          component={TotalMatchListTabScreen}
-        />
-        <Tab.Screen
-          name="MyMatchListTab"
-          options={{
-            tabBarLabel: 'My매칭',
-          }}
-          component={MyMatchListTabScreen}
-        />
-      </Tab.Navigator>
+      <SafeAreaView style={{backgroundColor: theme.palette['gray-0']}} />
+      <TopTabNavigator screens={screens} />
     </>
   );
 }
--- src/screens/record/RecordsScreen.tsx
+++ src/screens/record/RecordsScreen.tsx
@@ -1,41 +1,39 @@
+import React from 'react';
 import {SafeAreaView} from 'react-native';
 import {CompositeScreenProps} from '@react-navigation/native';
 import {BottomTabScreenProps} from '@react-navigation/bottom-tabs';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
-import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
-
 import {BottomTabStackParamList, RootStackParamList} from '../../navigators';
-
 import {RecordsTabScreen} from './RecordsTabScreen';
 import {SummaryTabScreen} from './SummaryTabScreen';
+
+import {theme} from '../../assets/styles/theme';
+
+import {TopTabNavigator} from '../../components/TopTabNavigator';
 
 type Props = CompositeScreenProps<
   BottomTabScreenProps<BottomTabStackParamList, 'Records'>,
   NativeStackScreenProps<RootStackParamList>
 >;
 
-const Tab = createMaterialTopTabNavigator();
+const screens = [
+  {
+    name: 'RecordsTab',
+    label: '운동 기록',
+    component: RecordsTabScreen,
+  },
+  {
+    name: 'SummaryTab',
+    label: '요약',
+    component: SummaryTabScreen,
+  },
+];
 
 export function RecordsScreen({navigation}: Props) {
   return (
     <>
-      <SafeAreaView style={{backgroundColor: '#ffffff'}} />
-      <Tab.Navigator>
-        <Tab.Screen
-          name="RecordsTab"
-          options={{
-            tabBarLabel: '운동 기록',
-          }}
-          component={RecordsTabScreen}
-        />
-        <Tab.Screen
-          name="SummaryTab"
-          options={{
-            tabBarLabel: '요약',
-          }}
-          component={SummaryTabScreen}
-        />
-      </Tab.Navigator>
+      <SafeAreaView style={{backgroundColor: theme.palette['gray-0']}} />
+      <TopTabNavigator screens={screens} />
     </>
   );
 }
