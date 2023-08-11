25ea686cc6391b2d144ef2ac5e160b5e295681e7
minimalKim <kimin3004@gmail.com>
2023-08-12 04:06:25 +0900

    🐛 fix: 변경된 디렉토리 import 반영

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import React from 'react';
 import {ThemeProvider as EmotionThemeProvider} from '@emotion/react';
 import {AppNavigator} from './navigators';
 import {theme} from './assets/styles/theme';
-import {initHealthKit} from './lib/AppleHealthKit';
+import {initHealthKit} from './lib/AppleHealthKit/AppleHealthKit';
 
 initHealthKit();
 
