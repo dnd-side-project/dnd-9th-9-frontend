d766998d78bc86ed9ec9bd3d5af50d59c80a49be
Minjeong Kim <80511900+minimalKim@users.noreply.github.com>
2023-08-12 22:27:44 +0900

    ✨ feat: RecordsTabScreen UI 적용 및 임시 데이터 연결 (#50)
    * ✨ feat: 임시 AppleHealthKit const 및 type 파일 추가
    
    * ✨ feat: WorkoutCardItem 컴포넌트 추가
    
    * ✨ feat: WorkoutCardGroup 컴포넌트 추가
    
    * ✨ feat: RecordsTabScreen에 컴포넌트 적용 및 데이터 연결
    
    * 🐛 fix: 변경된 디렉토리 import 반영
    
    * Revert "🐛 fix: 변경된 디렉토리 import 반영"
    
    This reverts commit 25ea686cc6391b2d144ef2ac5e160b5e295681e7.
    
    * ✨ feat: durationText 추가
    
    * 🎨 style: const 사용으로 변경
    
    * 🐛 fix: 누락 svg  추가
    
    ---------
    
    Co-authored-by: minimalKim <kimin3004@gmail.com>

---
+++ src/lib/AppleHealthKit/const.ts
@@ -0,0 +1,295 @@
+// TODO: backend에게 해당 데이터 공유 필요. API로 만들어질 시 제거 가능
+export const WORKOUT_ACTIVITY = {
+  AmericanFootball: {
+    label: '미식축구',
+    emoji: '⚽️',
+  },
+  Archery: {
+    label: '양궁',
+    emoji: '🏹',
+  },
+  AustralianFootball: {
+    label: '호주식축구',
+    emoji: '🏉',
+  },
+  Badminton: {
+    label: '배드민턴',
+    emoji: '🏸',
+  },
+  Baseball: {
+    label: '야구',
+    emoji: '⚾️',
+  },
+  Basketball: {
+    label: '농구',
+    emoji: '🏀',
+  },
+  Bowling: {
+    label: '볼링',
+    emoji: '🎳',
+  },
+  Boxing: {
+    label: '복싱',
+    emoji: '🥊',
+  },
+  CardioDance: {
+    label: '댄스',
+    emoji: '💃',
+  },
+  Climbing: {
+    label: '등산',
+    emoji: '🧗‍♂️',
+  },
+  Cricket: {
+    label: '크리켓',
+    emoji: '🏏',
+  },
+  CrossTraining: {
+    label: '크로스 트레이닝',
+    emoji: '🏋️‍♂️',
+  },
+  Curling: {
+    label: '컬링',
+    emoji: '🥌',
+  },
+  Cycling: {
+    label: '사이클링',
+    emoji: '🚴',
+  },
+  Dance: {
+    label: '댄스',
+    emoji: '💃',
+  },
+  Elliptical: {
+    label: '일립티컬',
+    emoji: '🚶‍♀️',
+  },
+  EquestrianSports: {
+    label: '승마',
+    emoji: '🐎',
+  },
+  Fencing: {
+    label: '펜싱',
+    emoji: '🤺',
+  },
+  Fishing: {
+    label: '낚시',
+    emoji: '🎣',
+  },
+  FunctionalStrengthTraining: {
+    label: '기능성 강도 훈련',
+    emoji: '🏋️‍♂️',
+  },
+  Golf: {
+    label: '골프',
+    emoji: '⛳️',
+  },
+  Gymnastics: {
+    label: '체조',
+    emoji: '🤸',
+  },
+  Handball: {
+    label: '핸드볼',
+    emoji: '🤾‍♂️',
+  },
+  Hiking: {
+    label: '하이킹',
+    emoji: '🚶‍♂️',
+  },
+  Hockey: {
+    label: '하키',
+    emoji: '🏒',
+  },
+  Hunting: {
+    label: '사냥',
+    emoji: '🦌',
+  },
+  Lacrosse: {
+    label: '라크로스',
+    emoji: '🥍',
+  },
+  MartialArts: {
+    label: '무술',
+    emoji: '🥋',
+  },
+  MindAndBody: {
+    label: '마음과 몸',
+    emoji: '🧘',
+  },
+  PaddleSports: {
+    label: '패들 스포츠',
+    emoji: '🚣',
+  },
+  Play: {
+    label: '놀이',
+    emoji: '🤹‍♂️',
+  },
+  PreparationAndRecovery: {
+    label: '준비 및 회복 운동',
+    emoji: '🧘',
+  },
+  Racquetball: {
+    label: '라켓볼',
+    emoji: '🏸',
+  },
+  Rowing: {
+    label: '로잉',
+    emoji: '🚣',
+  },
+  Rugby: {
+    label: '럭비',
+    emoji: '🏉',
+  },
+  Running: {
+    label: '러닝',
+    emoji: '🏃',
+  },
+  Sailing: {
+    label: '요트',
+    emoji: '⛵️',
+  },
+  SkatingSports: {
+    label: '스케이팅 스포츠',
+    emoji: '⛸',
+  },
+  SnowSports: {
+    label: '스노우 스포츠',
+    emoji: '🎿',
+  },
+  Soccer: {
+    label: '축구',
+    emoji: '⚽️',
+  },
+  Softball: {
+    label: '소프트볼',
+    emoji: '🥎',
+  },
+  Squash: {
+    label: '스쿼시',
+    emoji: '🎾',
+  },
+  StairClimbing: {
+    label: '계단 오르기',
+    emoji: '🏃',
+  },
+  SurfingSports: {
+    label: '서핑 스포츠',
+    emoji: '🏄‍♂️',
+  },
+  Swimming: {
+    label: '수영',
+    emoji: '🏊‍♂️',
+  },
+  TableTennis: {
+    label: '탁구',
+    emoji: '🏓',
+  },
+  Tennis: {
+    label: '테니스',
+    emoji: '🎾',
+  },
+  TrackAndField: {
+    label: '트랙 앤드 필드',
+    emoji: '🏃',
+  },
+  TraditionalStrengthTraining: {
+    label: '근력 강화 훈련',
+    emoji: '🏋️‍♂️',
+  },
+  Volleyball: {
+    label: '배구',
+    emoji: '🏐',
+  },
+  Walking: {
+    label: '걷기',
+    emoji: '🚶‍♂️',
+  },
+  WaterFitness: {
+    label: '수중 피트니스',
+    emoji: '🏊‍♂️',
+  },
+  WaterPolo: {
+    label: '수구',
+    emoji: '🤽‍♂️',
+  },
+  WaterSports: {
+    label: '수상 스포츠',
+    emoji: '🏄‍♂️',
+  },
+  Wrestling: {
+    label: '레슬링',
+    emoji: '🤼‍♂️',
+  },
+  Yoga: {
+    label: '요가',
+    emoji: '🧘‍♂️',
+  },
+  Barre: {
+    label: '바레',
+    emoji: '💃',
+  },
+  CoreTraining: {
+    label: '코어 트레이닝',
+    emoji: '🏋️‍♂️',
+  },
+  CrossCountrySkiing: {
+    label: '크로스컨트리 스키',
+    emoji: '🎿',
+  },
+  DownhillSkiing: {
+    label: '다운힐 스키',
+    emoji: '🎿',
+  },
+  Flexibility: {
+    label: '유연성',
+    emoji: '🤸',
+  },
+  HighIntensityIntervalTraining: {
+    label: '고강도 인터벌 트레이닝',
+    emoji: '🏋️‍♂️',
+  },
+  JumpRope: {
+    label: '줄넘기',
+    emoji: '🏃',
+  },
+  Kickboxing: {
+    label: '킥복싱',
+    emoji: '🥊',
+  },
+  Pilates: {
+    label: '필라테스',
+    emoji: '🧘‍♂️',
+  },
+  Snowboarding: {
+    label: '스노보드',
+    emoji: '🏂',
+  },
+  Stairs: {
+    label: '계단',
+    emoji: '🏃',
+  },
+  StepTraining: {
+    label: '스텝 트레이닝',
+    emoji: '🏋️‍♂️',
+  },
+  WheelchairWalkPace: {
+    label: '휠체어 걷기 속도',
+    emoji: '🚶‍♂️',
+  },
+  WheelchairRunPace: {
+    label: '휠체어 뛰기 속도',
+    emoji: '🏃',
+  },
+  TaiChi: {
+    label: '타이치',
+    emoji: '🧘‍♂️',
+  },
+  MixedCardio: {
+    label: '혼합 유산소 운동',
+    emoji: '🏃',
+  },
+  HandCycling: {
+    label: '핸드 사이클링',
+    emoji: '🚴',
+  },
+} as const;
