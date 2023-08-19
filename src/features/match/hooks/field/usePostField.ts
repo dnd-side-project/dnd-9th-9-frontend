06295fe36b245f1d9013d53e85d02aa48f6c41e2
chajuhui123 <chajuhui123@naver.com>
2023-08-19 16:08:44 +0900

    📝 chore: 매칭 생성 API body type 인터페이스 셋팅

--- src/features/match/hooks/field/usePostField.ts
+++ src/features/match/hooks/field/usePostField.ts
@@ -3,20 +3,10 @@ import {type UseMutationResult, useMutation} from '@tanstack/react-query';
 import {KEYS} from './keys';
 import {axios} from '../../../../lib/axios';
 import {queryClient} from '../../../../lib/react-query';
+import {type ICreateField} from '../../types';
 
 interface IProps {
-  body: {
-    description: string;
-    fieldType: string;
-    goal: string;
-    maxSize: number;
-    name: string;
-    period: string;
-    profileImg: string;
-    rule: string;
-    skillLevel: string;
-    strength: string;
-  };
+  body: ICreateField;
 }
 
 const fetcher = async ({body}: IProps): Promise<string> =>
