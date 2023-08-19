01146bb9427919c0514937ba0c96f9cc9539e262
chajuhui123 <chajuhui123@naver.com>
2023-08-19 16:07:51 +0900

    ✨ feat: 매칭 생성 폼 상태 관리 createMatchSlice 생성

--- src/store/client/useStore.ts
+++ src/store/client/useStore.ts
@@ -2,12 +2,14 @@ import {create} from 'zustand';
 
 import {type IAuthSlice, createAuthSlice} from './createAuthSlice';
 import {type ICounterSlice, createCounterSlice} from './createCounterSlice';
+import {type ICreateMatchSlice, createMatchSlice} from './createMatchSlice';
 
-type TStore = IAuthSlice & ICounterSlice;
+type TStore = IAuthSlice & ICounterSlice & ICreateMatchSlice;
 
 const useStore = create<TStore>()((...a) => ({
   ...createAuthSlice(...a),
   ...createCounterSlice(...a),
+  ...createMatchSlice(...a),
 }));
 
 export default useStore;
