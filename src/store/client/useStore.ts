27c1a52407b3fe6f5c71235b01a1a085e0b3ba99
chajuhui123 <chajuhui123@naver.com>
2023-08-20 19:38:47 +0900

    ✨ feat: 매칭 신청 내역 더보기에 대해 type을 관리하는 crateMatchDetailMatchingSlice 추가

--- src/store/client/useStore.ts
+++ src/store/client/useStore.ts
@@ -2,12 +2,17 @@ import {create} from 'zustand';
 
 import {type IAuthSlice, createAuthSlice} from './createAuthSlice';
 import {type ICounterSlice, createCounterSlice} from './createCounterSlice';
+import {
+  type IMatchDetailMatchingSlice,
+  crateMatchDetailMatchingSlice,
+} from './createMatchDetailSlice';
 
-type TStore = IAuthSlice & ICounterSlice;
+type TStore = IAuthSlice & ICounterSlice & IMatchDetailMatchingSlice;
 
 const useStore = create<TStore>()((...a) => ({
   ...createAuthSlice(...a),
   ...createCounterSlice(...a),
+  ...crateMatchDetailMatchingSlice(...a),
 }));
 
 export default useStore;
