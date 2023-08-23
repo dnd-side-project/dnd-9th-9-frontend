4e81fb4656907a4bb2540ff78ea8ac42bd2e6d52
chajuhui123 <chajuhui123@naver.com>
2023-08-23 17:01:48 +0900

    ✨ fix: MatchDetailRecordDetailScreen 비공개 메모 영역 UI 변경

--- src/screens/match/detail/record/MatchDetailRecordDetailScreen.tsx
+++ src/screens/match/detail/record/MatchDetailRecordDetailScreen.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 
 import styled from '@emotion/native';
 import {type RouteProp, useRoute} from '@react-navigation/native';
-import {SafeAreaView, View} from 'react-native';
+import {SafeAreaView, ScrollView, View} from 'react-native';
 
 import {theme} from '../../../../assets/styles/theme';
+import {lockXmlData} from '../../../../assets/svg';
 import {Gap} from '../../../../components/Gap';
+import {Icon} from '../../../../components/Icon';
 import {Line} from '../../../../components/Line';
 import {Text} from '../../../../components/Text';
 import {RecordDetailInformationItem} from '../../../../features/match/components/MatchDetailRecord';
@@ -44,79 +46,95 @@ export const MatchDetailRecordDetailScreen = (): React.JSX.Element => {
         flex: 1,
         backgroundColor: theme.palette['gray-0'],
       }}>
-      <StyledSectionWrapper>
-        <StyledFlexSection style={{justifyContent: 'space-between'}}>
-          <StyledFlexSection>
-            <StyledProfile style={{marginRight: 10}} />
-            <View>
-              <Text
-                type="body1"
-                color="gray-600"
-                fontWeight="600"
-                text={isLeader ? '방장' : '팀원'}
-              />
-              <Gap size="2px" />
-              <Text
-                type="body1"
-                color="gray-950"
-                fontWeight="600"
-                text={name}
-              />
-            </View>
+      <ScrollView>
+        <StyledSectionWrapper>
+          <StyledFlexSection style={{justifyContent: 'space-between'}}>
+            <StyledFlexSection>
+              <StyledProfile style={{marginRight: 10}} />
+              <View>
+                <Text
+                  type="body1"
+                  color="gray-600"
+                  fontWeight="600"
+                  text={isLeader ? '방장' : '팀원'}
+                />
+                <Gap size="2px" />
+                <Text
+                  type="body1"
+                  color="gray-950"
+                  fontWeight="600"
+                  text={name}
+                />
+              </View>
+            </StyledFlexSection>
+            <Text
+              type="body2"
+              color="gray-600"
+              fontWeight="400"
+              text={formattedExerciseDateTime}
+            />
           </StyledFlexSection>
-          <Text
-            type="body2"
-            color="gray-600"
-            fontWeight="400"
-            text={formattedExerciseDateTime}
+        </StyledSectionWrapper>
+
+        <Line size="lg" />
+
+        <StyledSectionWrapper>
+          {/* TODO: 상수로 변경 */}
+          <RecordDetailInformationItem
+            label="운동 종류"
+            value={WORKOUT_ACTIVITIES[sports].label}
           />
-        </StyledFlexSection>
-      </StyledSectionWrapper>
+          <Gap size="42px" />
+          <RecordDetailInformationItem
+            label="운동 시간"
+            value={`${durationHours}시간 ${durationRemainMinutes}분`}
+          />
+          <Gap size="42px" />
+          <RecordDetailInformationItem
+            label="예상 소비 칼로리"
+            value={`${burnedCalorie}kcal`}
+          />
+          <Gap size="37px" />
+        </StyledSectionWrapper>
 
-      <Line size="lg" />
+        <Line size="lg" />
 
-      <StyledSectionWrapper>
-        {/* TODO: 상수로 변경 */}
-        <RecordDetailInformationItem
-          label="운동 종류"
-          value={WORKOUT_ACTIVITIES[sports].label}
-        />
-        <Gap size="42px" />
-        <RecordDetailInformationItem
-          label="운동 시간"
-          value={`${durationHours}시간 ${durationRemainMinutes}분`}
-        />
-        <Gap size="42px" />
-        <RecordDetailInformationItem
-          label="예상 소비 칼로리"
-          value={`${burnedCalorie}kcal`}
-        />
-        <Gap size="37px" />
-      </StyledSectionWrapper>
+        <StyledSectionWrapper>
+          <Text type="head3" fontWeight="600" text="메모" />
+          {profileImg.length > 0 && isMemoPublic && (
+            <>
+              <Gap size="24px" />
+              <StyledMemoSection>
+                {/* TODO: img 연동 */}
+                <Text type="body2" fontWeight="400" text={memoImg} />
+              </StyledMemoSection>
+            </>
+          )}
 
-      <Line size="lg" />
-
-      <StyledSectionWrapper>
-        <Text type="head3" fontWeight="600" text="메모" />
-        {profileImg.length > 0 && isMemoPublic && (
-          <>
-            <Gap size="24px" />
-            <StyledMemoSection>
-              {/* TODO: img 연동 */}
-              <Text type="body2" fontWeight="400" text={memoImg} />
-            </StyledMemoSection>
-          </>
-        )}
-
-        {isMemoPublic && (
-          <>
-            <Gap size="24px" />
-            <StyledMemoSection>
-              <Text type="body2" fontWeight="400" text={memoContent} />
-            </StyledMemoSection>
-          </>
-        )}
-      </StyledSectionWrapper>
+          {isMemoPublic ? (
+            <>
+              <Gap size="24px" />
+              <StyledMemoSection style={{padding: 20}}>
+                <Text type="body2" fontWeight="400" text={memoContent} />
+              </StyledMemoSection>
+            </>
+          ) : (
+            <>
+              <Gap size="24px" />
+              <StyledMemoSection
+                style={{alignItems: 'center', paddingVertical: 56}}>
+                <Icon color={theme.palette['gray-400']} svgXml={lockXmlData} />
+                <Text
+                  type="body2"
+                  color="gray-400"
+                  fontWeight="400"
+                  text="비공개 입니다."
+                />
+              </StyledMemoSection>
+            </>
+          )}
+        </StyledSectionWrapper>
+      </ScrollView>
     </SafeAreaView>
   );
 };
@@ -140,5 +158,4 @@ const StyledFlexSection = styled.View`
 const StyledMemoSection = styled.View`
   border-radius: ${props => props.theme.borderRadius.md};
   background-color: ${props => props.theme.palette['gray-50']};
-  padding: 20px;
 `;
