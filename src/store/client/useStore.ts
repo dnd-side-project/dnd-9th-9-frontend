27dcac95a1866f7f85662472790cc533f57a735a
chajuhui123 <chajuhui123@naver.com>
2023-08-23 20:29:33 +0900

    📝 chore: createMathDetailMatchingSlice 제거

--- src/store/client/useStore.ts
+++ src/store/client/useStore.ts
@@ -2,19 +2,13 @@ import {create} from 'zustand';
 
 import {type IAuthSlice, createAuthSlice} from './createAuthSlice';
 import {type ICounterSlice, createCounterSlice} from './createCounterSlice';
-import {
-  type IMatchDetailMatchingSlice,
-  crateMatchDetailMatchingSlice,
-} from './createMatchDetailSlice';
-
 import {type IRecordSlice, createRecordSlice} from './createRecordSlice';
 
-type TStore = IAuthSlice & ICounterSlice & IRecordSlice & IMatchDetailMatchingSlice;
+type TStore = IAuthSlice & ICounterSlice & IRecordSlice;
 
 const useStore = create<TStore>()((...a) => ({
   ...createAuthSlice(...a),
   ...createCounterSlice(...a),
-  ...crateMatchDetailMatchingSlice(...a),
   ...createRecordSlice(...a),
 }));
 
