9d92dc4052672f7587012ad970d627c29689a83e
minimalKim <kimin3004@gmail.com>
2023-09-03 04:41:39 +0900

    📝 chore: 미사용 코드 제거

--- src/features/record/hooks/exercise/index.ts
+++ src/features/record/hooks/exercise/index.ts
@@ -1,5 +1,4 @@
 export * from './useGetExerciseList';
-export * from './useGetExerciseCalorie';
 export * from './useGetExerciseSummary';
 export * from './useGetRecentExercise';
 export * from './usePostAppleExercise';
