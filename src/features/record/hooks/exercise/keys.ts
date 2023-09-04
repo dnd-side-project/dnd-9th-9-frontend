92e42c29a0c5fd72ab79cc2bd047bc2aaf42b663
minimalKim <kimin3004@gmail.com>
2023-09-05 00:09:26 +0900

    ✨ feat: useGetBurnedCalorieGoal hook 추가

--- src/features/record/hooks/exercise/keys.ts
+++ src/features/record/hooks/exercise/keys.ts
@@ -3,6 +3,8 @@ export const KEYS = {
   list: (date: string) => [...KEYS.all, 'list', {date}] as const,
   detail: (id: number) => [...KEYS.all, 'detail', id] as const,
   calorie: (date: string) => [...KEYS.all, 'calorie', {date}] as const,
+  burnedCalorieGoal: (date: string) =>
+    [...KEYS.all, 'burned-calorie-goal', {date}] as const,
   summary: (date: string) => [...KEYS.all, 'summary', {date}] as const,
   recent: (date: string) => [...KEYS.all, 'recent', {date}] as const,
 };
