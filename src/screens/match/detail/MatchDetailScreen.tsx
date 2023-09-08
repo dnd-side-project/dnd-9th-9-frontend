6926c0e64883a2d97d93f7975c7ac29a53d039cc
chajuhui123 <chajuhui123@naver.com>
2023-09-08 17:49:00 +0900

    ✨ feat: 매칭 생성 이후 매칭 상세 페이지 navigation 연동

--- src/screens/match/detail/MatchDetailScreen.tsx
+++ src/screens/match/detail/MatchDetailScreen.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import styled from '@emotion/native';
 import {type RouteProp, useRoute} from '@react-navigation/native';
 import {type NativeStackScreenProps} from '@react-navigation/native-stack';
-import {SafeAreaView} from 'react-native';
+import {SafeAreaView, View} from 'react-native';
 
 import {MatchDetailMemberScreen} from './MatchDetailMemberScreen';
 import {MatchDetailProfileScreen} from './MatchDetailProfileScreen';
@@ -15,6 +15,7 @@ import {
   type ITopTabScreen,
   TopTabNavigator,
 } from '../../../components/TopTabNavigator';
+import {useGetFieldDetail} from '../../../features/match/hooks/field';
 import {type MatchStackParamList} from '../../../navigators/MatchNavigator';
 
 type TMatchDetailScreenProps = NativeStackScreenProps<
@@ -33,28 +34,63 @@ export const MatchDetailScreen = ({
   const route = useRoute<TMatchDetailScreenRouteProps>();
   const {id} = route.params;
 
-  const screens: ITopTabScreen[] = [
-    {
-      name: 'TeamProfile',
-      label: '프로필',
-      component: () => <MatchDetailProfileScreen id={id} />,
-    },
-    {
-      name: 'TeamRecord',
-      label: '기록',
-      component: () => <MatchDetailRecordScreen id={id} />,
-    },
-    {
-      name: 'TeamMatching',
-      label: '매칭',
-      component: MatchDetailMatchingScreen,
-    },
-    {
-      name: 'TeamMember',
-      label: '팀원',
-      component: MatchDetailMemberScreen,
-    },
-  ];
+  // TODO: ERR 처리
+  if (id === undefined) return <View></View>;
+
+  // TODO: 다른 접근에 대해서도 처리 (생성 후 프로필 상세 등)
+  const {data: fieldDetailData} = useGetFieldDetail({
+    id,
+  });
+
+  const getScreenByRule = (): ITopTabScreen[] => {
+    const userRole = fieldDetailData?.fieldDto?.fieldRole;
+
+    switch (userRole) {
+      case 'MEMBER':
+      case 'LEADER':
+        return [
+          {
+            name: 'TeamProfile',
+            label: '프로필',
+            component: () => (
+              <MatchDetailProfileScreen fieldDetailData={fieldDetailData} />
+            ),
+          },
+          {
+            name: 'TeamRecord',
+            label: '기록',
+            component: () => <MatchDetailRecordScreen id={id} />,
+          },
+          {
+            name: 'TeamMatching',
+            label: '매칭',
+            component: MatchDetailMatchingScreen,
+          },
+          {
+            name: 'TeamMember',
+            label: '팀원',
+            component: MatchDetailMemberScreen,
+          },
+        ];
+      default:
+        return [
+          {
+            name: 'TeamProfile',
+            label: '프로필',
+            component: () => (
+              <MatchDetailProfileScreen fieldDetailData={fieldDetailData} />
+            ),
+          },
+          {
+            name: 'TeamMember',
+            label: '팀원',
+            component: MatchDetailMemberScreen,
+          },
+        ];
+    }
+  };
+
+  const screens = getScreenByRule();
 
   return (
     <>
