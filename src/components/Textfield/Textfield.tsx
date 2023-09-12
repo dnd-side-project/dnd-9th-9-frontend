880a7cd50f939f7b2d9306b333d630fa8f410c88
minimalKim <kimin3004@gmail.com>
2023-09-13 02:12:07 +0900

    ✨ feat: Textfield 공통 컴포넌트 추가

---
+++ src/components/Textfield/Textfield.tsx
@@ -0,0 +1,93 @@
+import React, {useState} from 'react';
+
+import styled from '@emotion/native';
+import {type TextInputProps} from 'react-native';
+
+import {theme} from '../../assets/styles/theme';
+import {checkXmlData, errorXmlData} from '../../assets/svg';
+import {Icon} from '../Icon';
+import {Text} from '../Text/Text';
+
+interface ITextfieldProps extends TextInputProps {
+  isError?: boolean;
+  isValid?: boolean;
+  label?: string;
+  hintMessage?: string;
+  errorMessage?: string;
+  rightElement?: () => React.JSX.Element;
+}
+
+export const Textfield = ({
+  isError = false,
+  isValid = false,
+  label = '',
+  hintMessage = '',
+  errorMessage = '',
+  rightElement,
+  ...props
+}: ITextfieldProps): React.JSX.Element => {
+  const [isFocused, setIsFocused] = useState(false);
+
+  const rightIcon = isError ? errorXmlData : isValid ? checkXmlData : null;
+  return (
+    <StyledTextfield>
+      <Text text={label} type="caption" color="gray-600" />
+
+      <StyledTextInputContainer isError={isError} isFocused={isFocused}>
+        <StyledTextInput
+          placeholderTextColor={theme.palette['gray-100']}
+          onFocus={() => {
+            setIsFocused(true);
+          }}
+          onEndEditing={() => {
+            // onBlur이 동작하지 않는 이슈
+            setIsFocused(false);
+          }}
+          {...props}
+        />
+
+        {rightIcon != null && (
+          <Icon svgXml={rightIcon} width={40} height={40} />
+        )}
+        {rightElement?.()}
+      </StyledTextInputContainer>
+
+      {errorMessage !== '' ? (
+        <Text type="caption" color="error-light" text={errorMessage} />
+      ) : (
+        <Text type="caption" color="gray-600" text={hintMessage} />
+      )}
+    </StyledTextfield>
+  );
+};
+
+const StyledTextfield = styled.View`
+  display: flex;
+  gap: 4px;
+`;
+
+const StyledTextInputContainer = styled.View<{
+  isError: boolean;
+  isFocused: boolean;
+}>`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  border-bottom-color: ${({isError, isFocused, theme}) =>
+    isError
+      ? theme.palette['error-light']
+      : isFocused
+      ? theme.palette.black
+      : theme.palette['gray-100']};
+  border-bottom-width: 2px;
+  transition: all 1s ease-in-out;
+`;
+
+const StyledTextInput = styled.TextInput`
+  flex: 1;
+  padding: 10px 0;
+  color: ${({theme}) => theme.palette.black};
+  font-family: Pretendard;
+  font-size: 16px;
+  font-weight: 700;
+`;
