ece64075a8763ed382e7dc58b2115599bf938565
chajuhui123 <chajuhui123@naver.com>
2023-09-23 22:15:49 +0900

    ✨ feat: MY 매칭 화면 MyMatchListScreen 셋팅

---
+++ src/screens/match/my/MyMatchListScreen.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import {ScrollView} from 'react-native';
+
+import {MyMatchApplicationScreen} from './MyMatchApplicationScreen';
+import {MyMatchCompleteScreen} from './MyMatchCompleteScreen';
+import {MyMatchProgressScreen} from './MyMatchProgressScreen';
+import {theme} from '../../../assets/styles/theme';
+import {MyMatchStatusFilter} from '../../../features/match/components/MyMatch/MyMatchStatusFilter';
+import {type TMyMatchStatus} from '../../../features/match/types';
+
+interface IMyMatchListScreenProps {
+  matchStatus: TMyMatchStatus;
+}
+
+export const MyMatchListScreen = ({
+  matchStatus,
+}: IMyMatchListScreenProps): React.JSX.Element => {
+  const getMyMatchScreen = {
+    APPLICATION: <MyMatchApplicationScreen />,
+    PROGRESS: <MyMatchProgressScreen />,
+    COMPLETE: <MyMatchCompleteScreen />,
+  };
+
+  return (
+    <ScrollView style={{flex: 1, backgroundColor: theme.palette['gray-0']}}>
+      <MyMatchStatusFilter matchStatus={matchStatus} />
+      {getMyMatchScreen[matchStatus]}
+    </ScrollView>
+  );
+};
