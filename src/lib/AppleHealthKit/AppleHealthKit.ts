9034c3740706e0f3af208f8f4b8deb3d416e8eb7
minimalKim <kimin3004@gmail.com>
2023-10-03 18:26:53 +0900

    ✨ feat: healthKit 관련 메소드를 promise를 반환하도록 수정  및 사용 메소드 추가

--- src/lib/AppleHealthKit/AppleHealthKit.ts
+++ src/lib/AppleHealthKit/AppleHealthKit.ts
@@ -1,29 +1,105 @@
 import {NativeEventEmitter, NativeModules} from 'react-native';
-import AppleHealthKit, {type HealthKitPermissions} from 'react-native-health';
+import AppleHealthKit, {
+  type HealthStatusResult,
+  type HealthKitPermissions,
+  type HealthUnit,
+  type HealthValue,
+  type HealthActivitySummary,
+  type HealthInputOptions,
+} from 'react-native-health';
 
 // https://github.com/agencyenterprise/react-native-health
 
-export const initHealthKit = (): void => {
-  const permissions: HealthKitPermissions = {
-    permissions: {
-      read: [
-        AppleHealthKit.Constants.Permissions.ActivitySummary,
-        AppleHealthKit.Constants.Permissions.ActiveEnergyBurned,
-        AppleHealthKit.Constants.Permissions.AppleExerciseTime,
-        AppleHealthKit.Constants.Permissions.Workout,
-      ],
-      write: [AppleHealthKit.Constants.Permissions.Workout],
-    },
-  };
+const permissions: HealthKitPermissions = {
+  permissions: {
+    read: [
+      AppleHealthKit.Constants.Permissions.ActivitySummary,
+      AppleHealthKit.Constants.Permissions.ActiveEnergyBurned,
+      AppleHealthKit.Constants.Permissions.AppleExerciseTime,
+      AppleHealthKit.Constants.Permissions.Workout,
+      AppleHealthKit.Constants.Permissions.Weight,
+      AppleHealthKit.Constants.Permissions.Height,
+      AppleHealthKit.Constants.Permissions.BiologicalSex,
+    ],
+    write: [AppleHealthKit.Constants.Permissions.Workout],
+  },
+};
 
-  AppleHealthKit.initHealthKit(permissions, (error: string) => {
-    if (error !== '') {
-      // TODO: error screen 추가 필요
-      console.log('[ERROR] Cannot grant permissions!');
-    }
-    console.log('initHealthKit');
+export const initHealthKit = async (): Promise<void> => {
+  await new Promise((resolve, reject) => {
+    AppleHealthKit.initHealthKit(permissions, (error: string) => {
+      // NOTE: error = object | null
+      if (error != null) {
+        // TODO: error screen 추가 필요
+        reject(new Error(error));
+      } else {
+        console.log('initHealthKit');
+      }
+    });
   });
 };
+
+export const getAuthStatus = async (): Promise<HealthStatusResult> =>
+  await new Promise((resolve, reject) => {
+    AppleHealthKit.getAuthStatus(permissions, (error, results) => {
+      if (error != null) {
+        reject(new Error(error));
+      } else {
+        resolve(results);
+      }
+    });
+  });
+
+export const getLatestHeight = async (): Promise<HealthValue> =>
+  await new Promise((resolve, reject) => {
+    AppleHealthKit.getLatestHeight({}, (error, results) => {
+      if (error != null) {
+        reject(new Error(''));
+      } else {
+        resolve(results);
+      }
+    });
+  });
+
+export const getLatestWeight = async (): Promise<HealthValue> =>
+  await new Promise((resolve, reject) => {
+    AppleHealthKit.getLatestWeight(
+      {unit: 'gram' as HealthUnit},
+      (error, results) => {
+        if (error != null) {
+          reject(new Error(error));
+        } else {
+          resolve(results);
+        }
+      },
+    );
+  });
+
+export const getBiologicalSex = async (): Promise<{value: string}> =>
+  await new Promise((resolve, reject) => {
+    AppleHealthKit.getBiologicalSex({}, (error, results) => {
+      if (error != null) {
+        reject(new Error(error));
+      } else {
+        // NOTE: results = {value: string}
+        resolve(results as unknown as {value: string});
+      }
+    });
+  });
+
+export const getActivitySummary = async (
+  options: HealthInputOptions = {startDate: new Date().toISOString()},
+): Promise<HealthActivitySummary[]> =>
+  await new Promise((resolve, reject) => {
+    AppleHealthKit.getActivitySummary(options, (error, results) => {
+      if (error != null) {
+        console.log(error);
+        reject(new Error(error));
+      } else {
+        resolve(results);
+      }
+    });
+  });
 
 type TObserverCallback = () => void;
 
