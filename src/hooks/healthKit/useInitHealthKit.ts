7fce0aeb1039e8e49f78c5aaf88b4aab2aa72346
minimalKim <kimin3004@gmail.com>
2023-10-04 23:04:00 +0900

    ✨ feat: useInitHealthKit 추가

---
+++ src/hooks/healthKit/useInitHealthKit.ts
@@ -0,0 +1,22 @@
+import {useMutation, type UseMutationResult} from '@tanstack/react-query';
+
+import {KEYS} from './keys';
+import {
+  type HealthActivityOptions,
+  initHealthKit,
+} from '../../lib/AppleHealthKit';
+import {queryClient} from '../../lib/react-query';
+
+export const useInitHealthKit = (): UseMutationResult<
+  void,
+  Error,
+  HealthActivityOptions
+> => {
+  return useMutation({
+    mutationFn: initHealthKit,
+    onSuccess: () => {
+      void queryClient.invalidateQueries(KEYS.auth());
+      console.log('init HealthKit');
+    },
+  });
+};
