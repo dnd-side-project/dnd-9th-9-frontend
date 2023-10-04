a66cb5ace06b1216ed840df634459bdcc1a3ea8c
minimalKim <kimin3004@gmail.com>
2023-10-04 23:38:59 +0900

    ✨ feat: healthKit 관련 hook 추가

--- src/hooks/healthKit/keys.ts
+++ src/hooks/healthKit/keys.ts
@@ -1,4 +1,11 @@
 export const KEYS = {
   all: ['health-kit'] as const,
+
   auth: () => [...KEYS.all, 'auth'] as const,
+
+  latestHeight: () => [...KEYS.all, 'latest-height'] as const,
+  latestWeight: () => [...KEYS.all, 'latest-weight'] as const,
+  biologicalSex: () => [...KEYS.all, 'biological-sex'] as const,
+
+  activitySummary: () => [...KEYS.all, 'activity-summary'] as const,
 };
---
+++ src/hooks/healthKit/useGetBiologicalSex.ts
@@ -0,0 +1,11 @@
+import {useQuery, type UseQueryResult} from '@tanstack/react-query';
+
+import {KEYS} from './keys';
+import {getBiologicalSex} from '../../lib/AppleHealthKit';
+
+export const useGetBiologicalSex = (): UseQueryResult<string | null, Error> =>
+  useQuery({
+    queryKey: KEYS.biologicalSex(),
+    queryFn: async () => await getBiologicalSex().then(({value}) => value),
+    initialData: null,
+  });
