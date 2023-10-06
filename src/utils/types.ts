415aaa2e8f01972c0fd74e3d9e039457f2809cf8
chajuhui123 <chajuhui123@naver.com>
2023-10-06 22:32:00 +0900

    ✨ feat: CustomAxiosError 타입 셋팅 및 API 연동

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,3 +1,5 @@
+import {type AxiosError} from 'axios';
+
 export type ValueOf<T> = T[keyof T];
 
 export type Entries<T> = Array<
@@ -5,3 +7,11 @@ export type Entries<T> = Array<
     [K in keyof T]: [K, T[K]];
   }[keyof T]
 >;
+
+export interface CustomAxiosError
+  extends AxiosError<{
+    code: string;
+    errors: any[];
+    message: string;
+    status: string;
+  }> {}
