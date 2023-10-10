23406381597b0bf294f701f1f72983bfb4cdc0bb
minimalKim <kimin3004@gmail.com>
2023-10-11 01:04:23 +0900

    🎨 style: 코드 스타일 수정

--- src/features/home/types/userField.ts
+++ src/features/home/types/userField.ts
@@ -3,39 +3,29 @@ import {type TFieldType} from '../../match/types';
 export type TUserFieldBattleType = Exclude<TFieldType, 'TEAM'>;
 
 export interface IUserFieldHomeTeam {
-  /**
-   * 소모 칼로리
-   */
-  burnedCalorie: {
-    name: string;
-    value: number;
-  };
-  /**
-   * 완료까지 남은 날짜
-   */
-  daysLeft: number;
-  /**
-   * 운동시간
-   */
-  exerciseTimeMinute: {
-    name: string;
-    value: number;
-  };
-  /**
-   * 운동시간
-   */
-  fieldId: number;
-  /**
-   * 운동시간
-   */
+  /** 활동링 달성 횟수 */
   goalAchievedCount: {
     name: string;
     value: number;
   };
+  /** 소모 칼로리 */
+  burnedCalorie: {
+    name: string;
+    value: number;
+  };
+  /** 소모 운동 시간 */
+  exerciseTimeMinute: {
+    name: string;
+    value: number;
+  };
+  /** 기록 횟수 */
   recordCount: {
     name: string;
     value: number;
   };
+  /** 완료까지 남은 날짜 */
+  daysLeft: number;
+  fieldId: number;
   teamName: string;
 }
 
