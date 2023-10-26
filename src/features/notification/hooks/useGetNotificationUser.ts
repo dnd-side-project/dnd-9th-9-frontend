5b8159effa534cb3ce1f04b5d25aad7500d9c137
minimalKim <kimin3004@gmail.com>
2023-10-27 02:55:15 +0900

    📝 chore: dummy data 제거

--- src/features/notification/hooks/useGetNotificationUser.ts
+++ src/features/notification/hooks/useGetNotificationUser.ts
@@ -5,46 +5,7 @@ import {
 
 import {KEYS} from './keys';
 import {axios} from '../../../lib/axios';
-import {dayjs} from '../../../lib/dayjs';
 import {type INotificationResponse, type IHomeNotification} from '../types';
-
-interface DummyData {
-  currentPageNumber: number;
-  currentPageSize: number;
-  totalCount: number;
-  notificationInfos: IHomeNotification[];
-}
-
-function generateDummyData(
-  pageNumber: number = 1,
-  pageSize: number = 10,
-): DummyData {
-  const dummyData: DummyData = {
-    currentPageNumber: pageNumber,
-    currentPageSize: pageSize,
-    totalCount: 0,
-    notificationInfos: [] as any[],
-  };
-
-  const startDate = dayjs('2023-11-26 12:11:02', {
-    format: 'YYYY-MM-DD HH:mm:ss',
-  });
-  const endDate = dayjs(startDate).add(pageNumber - 1, 'day');
-
-  for (let i = 1; i <= pageSize; i++) {
-    const newNotification = {
-      content: `Notification ${i}`,
-      createdAt: endDate.format('YYYY-MM-DD HH:mm:ss'),
-      fieldId: 100 + i + pageNumber,
-      id: Date.now(),
-      isRead: pageNumber > 2,
-    };
-    dummyData.notificationInfos.push(newNotification);
-    startDate.add(1, 'day');
-  }
-
-  return dummyData;
-}
 
 export interface IGetNotificationUserParams {
   offset?: number;
@@ -57,11 +18,7 @@ type TGetNotificationUserResponse = INotificationResponse<IHomeNotification>;
 const fetcher = async (
   params: IGetNotificationUserParams,
 ): Promise<TGetNotificationUserResponse> =>
-  await axios
-    .get('/notification/user', {
-      params,
-    })
-    .then(() => generateDummyData(params.pageNumber, params.pageSize));
+  await axios.get('/notification/user', {params});
 
 export const useGetNotificationUser = (
   params: IGetNotificationUserParams,
