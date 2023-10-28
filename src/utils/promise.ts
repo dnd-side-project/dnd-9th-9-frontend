ae2050896df75f140aa29099d3db715394e74ff9
chajuhui123 <chajuhui123@naver.com>
2023-10-28 10:20:28 +0900

    🐛 fix: sleep 함수 type 수정

--- src/utils/promise.ts
+++ src/utils/promise.ts
@@ -1,3 +1,4 @@
 /** ms 초의 delay를 주는 함수 */
-export const sleep = (ms: number): any =>
-  new Promise(resolve => setTimeout(resolve, ms));
+export const sleep = async (ms: number): Promise<void> => {
+  await new Promise<void>(resolve => setTimeout(resolve, ms));
+};
