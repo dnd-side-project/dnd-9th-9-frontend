578f833e743076b381a37670dea9412ecf4ed882
minimalKim <kimin3004@gmail.com>
2023-11-02 22:05:42 +0900

    ✨ feat: Line 컴포넌트 color props 추가

--- src/components/Line/Line.tsx
+++ src/components/Line/Line.tsx
@@ -2,18 +2,26 @@ import React from 'react';
 
 import styled from '@emotion/native';
 
+import {type theme} from '../../assets/styles/theme';
+
 export interface ILineProps {
   size: 'sm' | 'lg';
+  color?: keyof typeof theme.palette;
 }
 
-interface IStyledLine extends ILineProps {}
+interface IStyledLine extends ILineProps {
+  color: keyof typeof theme.palette;
+}
 
-export const Line = ({size}: ILineProps): React.JSX.Element => {
-  return <StyledLine size={size} />;
+export const Line = ({
+  size,
+  color = 'gray-50',
+}: ILineProps): React.JSX.Element => {
+  return <StyledLine size={size} color={color} />;
 };
 
 const StyledLine = styled.View<IStyledLine>`
-  background-color: ${props => props.theme.palette['gray-50']};
+  background-color: ${({theme, color}) => theme.palette[color]};
   height: ${props => (props.size === 'sm' ? '2px' : '14px')};
   width: 100%;
 `;
