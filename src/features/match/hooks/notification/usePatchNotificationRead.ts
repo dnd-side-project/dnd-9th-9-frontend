3d3bbe15aeb455bcdc68976df4b0517e3aa6a981
chajuhui123 <chajuhui123@naver.com>
2023-11-30 18:23:53 +0900

    ✨ feat: 매칭 상세 알림 화면 MatchDetailNotificationScreen 생성 및 API 연동

--- src/features/match/hooks/notification/usePatchNotificationRead.ts
+++ src/features/match/hooks/notification/usePatchNotificationRead.ts
@@ -9,20 +9,33 @@ interface IUsePatchNotificationUserReadQuery {
   id: number;
 }
 
+interface IUsePatchNotificationReadProps {
+  onSuccessCallback: () => void;
+  onErrorCallback: (error: CustomAxiosError) => void;
+}
+
 const fetcher = async ({
   id,
 }: IUsePatchNotificationUserReadQuery): Promise<string> =>
   await axios.patch(`/notification/${id}/read`).then(({data}) => data);
 
-export const usePatchNotificationRead = (): UseMutationResult<
+export const usePatchNotificationRead = ({
+  onSuccessCallback,
+  onErrorCallback,
+}: IUsePatchNotificationReadProps): UseMutationResult<
   string,
   CustomAxiosError,
   IUsePatchNotificationUserReadQuery
 > => {
   return useMutation({
     mutationFn: fetcher,
+
     onSuccess: () => {
+      onSuccessCallback();
       void queryClient.invalidateQueries(KEYS.all);
+    },
+    onError: error => {
+      onErrorCallback(error);
     },
   });
 };
