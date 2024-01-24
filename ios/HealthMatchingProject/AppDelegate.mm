328c6822f3031e5a73bb4701a97338954df6d4b5
chajuhui123 <chajuhui123@naver.com>
2024-01-24 23:46:27 +0900

    ✨ feat: ios firebase 앱등록

--- ios/HealthMatchingProject/AppDelegate.mm
+++ ios/HealthMatchingProject/AppDelegate.mm
@@ -3,6 +3,7 @@
 #import <React/RCTBundleURLProvider.h>
 
 #import "RCTAppleHealthKit.h"
+#import <Firebase.h>
 
 #import "RNSplashScreen.h"
 
@@ -10,6 +11,8 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [FIRApp configure];
+
   self.moduleName = @"HealthMatchingProject";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
